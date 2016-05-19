7a5671e05a65f2d8703757d6fa21c206af94a2eb
chirva-ivan <pooeblos@gmail.com>
2016-05-19 22:26:19 +0300

    Gulp local server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 var gulp = require('gulp'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect'),
 	notify = require('gulp-notify');
 
-gulp.task('default', function() {
-	gulp.src("")
-  		.pipe(notify("Gulp Done!"));
+gulp.task('connect', function() {
+  connect.server({
+  	root: "app",
+    livereload: true
+  });
 });
+
+gulp.task('html', function () {
+  gulp.src('app/weather.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['app/weather.html'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
